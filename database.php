f78b4b8107f89f17e90fed5691e85d8ffc08168e
Luiz Mendes <developer.luizmendes@gmail.com>
2019-03-07 17:43:54 -0300

    database.php

---
+++ database.php
@@ -0,0 +1,31 @@
+<?php
+return [
+    //...
+    'default' => env('DB_CONNECTION', 'mysql'),
+    //...
+    'connections' => [
+        'mysql' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'kuttnernobake'),
+            'username'  => env('DB_USERNAME', 'root'),
+            'password'  => env('DB_PASSWORD', ''),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
+        'testing' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'kuttnernobake'),
+            'username'  => env('DB_USERNAME', 'root'),
+            'password'  => env('DB_PASSWORD', ''),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
+    ],
+    //...
+];
