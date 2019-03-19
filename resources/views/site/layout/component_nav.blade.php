02e8ad2d96dd09da87d328782e1ef2394f6f019e
Luiz Mendes <developer.luizmendes@gmail.com>
2019-03-19 11:14:09 -0300

    alteração do nav, foram removidos alguns dropdowns

--- resources/views/site/layout/component_nav.blade.php
+++ resources/views/site/layout/component_nav.blade.php
@@ -1,16 +1,10 @@
 <div class="navbar-collapse collapse clearfix">
     <ul class="navigation clearfix">
-        <li @if($current=="inicio") class="current dropdown" @else class="dropdown" @endif>
-            <a href="#">Início</a>
-            <ul>
-                <li><a href="/">Início</a></li>
-            </ul>
+        <li @if($current=="inicio") class="current" @else class="" @endif>
+            <a href="/">Início</a>
         </li>
-        <li @if($current=="sobre") class="current dropdown" @else class="dropdown" @endif >
+        <li @if($current=="sobre") class="current" @else class="" @endif >
         <a href="#">Sobre nós</a>
-            <ul>
-                <li><a href="/sobre">Sobre nós</a></li>
-            </ul>
         </li>
         <li @if($current=="produtos") class="current dropdown" @else class="dropdown" @endif >
         <a href="#">Produtos</a>
@@ -18,11 +12,8 @@
                 <li><a href="/produtos">Produtos</a></li>
             </ul>
         </li>
-        <li @if($current=="projetos") class="current dropdown" @else class="dropdown" @endif >
+        <li @if($current=="projetos") class="current" @else class="" @endif >
         <a href="#">Projetos</a>
-            <ul>
-                <li><a href="/projetos">Projetos</a></li>
-            </ul>
         </li>
         <li @if($current=="contato") class="current dropdown" @else class="dropdown" @endif >
         <a href="#">Contato</a>
