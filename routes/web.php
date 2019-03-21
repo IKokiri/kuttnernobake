eb169c27632849dfc3b28c009757943f7b81d84d
Luiz Mendes <developer.luizmendes@gmail.com>
2019-03-21 17:23:03 -0300

    adicionado auth para as rotas de administração e criação do registro de usuários

--- routes/web.php
+++ routes/web.php
@@ -41,35 +41,43 @@ Route::get('contato/grupokuttner', function () {
     return view('site/contatokgb');
 });
 
-
 Route::get('projeto/', function () {
     return view('site/projeto');
 });
 
-Route::get('admin/', function () {
-    return view('admin/inicio');
+
+Route::middleware('auth')->group(function(){
+
+    Route::get('admin/', function () {
+        return view('admin/inicio');
+    })->middleware('auth');
+    
+    
+    Route::get('admin/newsletter','NewsLetter@index');
+    Route::get('admin/newsletter/apagar/{id}','NewsLetter@destroy');
+    Route::get('admin/newsletter/csv/','NewsLetter@csv');
+    
+    Route::get('admin/categoria','CategoryController@index');
+    Route::post('admin/categoria/','CategoryController@store');
+    Route::get('admin/categoria/{id}/editar','CategoryController@edit');
+    Route::put('admin/categoria/{id}','CategoryController@update');
+    Route::delete('admin/categoria/apagar/{id}','CategoryController@destroy');
+    
+    Route::get('admin/produtos','ProductController@index');
+    Route::post('admin/produtos/','ProductController@store');
+    Route::get('admin/produtos/{id}/editar','ProductController@edit');
+    Route::put('admin/produtos/{id}','ProductController@update');
+    Route::delete('admin/produtos/{id}','ProductController@destroy');
+    Route::get('admin/produtos/{id}/buscarImagens','ProductController@buscarImagens');
+    
+    Route::post('admin/produtoImagem','ProductImageController@store');
+    Route::delete('admin/produtoImagem/{id}','ProductImageController@destroy');
+    Route::get('admin/produtoImagem/{id}','ProductImageController@destroy');
 });
 
 
-Route::get('admin/newsletter','NewsLetter@index');
-Route::get('admin/newsletter/apagar/{id}','NewsLetter@destroy');
-Route::get('admin/newsletter/csv/','NewsLetter@csv');
-
-Route::get('admin/categoria','CategoryController@index');
-Route::post('admin/categoria/','CategoryController@store');
-Route::get('admin/categoria/{id}/editar','CategoryController@edit');
-Route::put('admin/categoria/{id}','CategoryController@update');
-Route::delete('admin/categoria/apagar/{id}','CategoryController@destroy');
-
-Route::get('admin/produtos','ProductController@index');
-Route::post('admin/produtos/','ProductController@store');
-Route::get('admin/produtos/{id}/editar','ProductController@edit');
-Route::put('admin/produtos/{id}','ProductController@update');
-Route::delete('admin/produtos/{id}','ProductController@destroy');
-Route::get('admin/produtos/{id}/buscarImagens','ProductController@buscarImagens');
-
-Route::post('admin/produtoImagem','ProductImageController@store');
-Route::delete('admin/produtoImagem/{id}','ProductImageController@destroy');
-Route::get('admin/produtoImagem/{id}','ProductImageController@destroy');
 
 
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
