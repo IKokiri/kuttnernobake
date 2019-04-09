305962ea6f05b194271509c4dde41fddd0e4ac14
Luiz Mendes <developer.luizmendes@gmail.com>
2019-04-09 08:35:10 -0300

    criado model/controller company

--- resources/views/admin/layout/component_sidebar.blade.php
+++ resources/views/admin/layout/component_sidebar.blade.php
@@ -29,6 +29,19 @@
 
 <!-- Nav Item - Pages Collapse Menu -->
 <li class="nav-item">
+  <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true"
+    aria-controls="collapseOne">
+    <i class="fas fa-fw fa-cog"></i>
+    <span>Informações</span>
+  </a>
+  <div id="collapseOne" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+    <div class="bg-white py-2 collapse-inner rounded">
+      <h6 class="collapse-header">Informações:</h6>
+      <a class="collapse-item" href="/admin/empresa">Empresa</a>
+    </div>    
+  </div>
+</li>
+<li class="nav-item">
   <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
     aria-controls="collapseTwo">
     <i class="fas fa-fw fa-cog"></i>
@@ -43,6 +56,7 @@
       <a class="collapse-item" href="/admin/projetos">Projetos</a>
       <a class="collapse-item" href="/admin/clientes">Clientes</a>
     </div>
+    
   </div>
 </li>
 
