40f48f377a1e12ed3e1a344e65c559ce64be0cb0
Luiz Mendes <developer.luizmendes@gmail.com>
2019-05-13 13:37:42 -0300

    adição de compos no formulario de orçamento

--- resources/views/site/layout/component_email.blade.php
+++ resources/views/site/layout/component_email.blade.php
@@ -14,7 +14,6 @@
                 <div class="inner-column">
                     <div class="sec-title">
                         <h2>Entre em contato</h2>
-                        <div class="text">Kuttner No-Bake Solutions</div>
                     </div>
                     <!--Contact Form-->
                     <div class="contact-form style-two">
@@ -36,21 +35,34 @@
                                         <input type="email" name="email" value="" placeholder="Email*"
                                             required>
                                     </div>
-
-                                    <div class="form-group">
-                                    <button type="submit" class="theme-btn">Enviar</button>
-                                    </div>
+                                    
                                 </div>
                                 <div class="column col-md-6 col-sm-6 col-xs-12">
+                                    
                                     <div class="form-group">
-                                        <textarea name="message" placeholder="Mensagem"><?= isset($_GET['produto'])?"Boa Tarde.\n\nGostaria de receber um orçamento referente ao produto ".explode("-",$_GET['produto'])[1]." com as seguintes especificações:\n\nObrigado(a).":'' ?></textarea>
+                                        <input type="text" name="telefone" value=""
+                                            placeholder="(19) 99999-9999" required>
                                     </div>
+                                    <div class="form-group">
+                                        <input type="text" name="empresa" value="" placeholder="Empresa"
+                                            required>
+                                    </div>
+                                    
                                 </div>
+                              
                                 <!-- <div class="column col-md-12 col-sm-6 col-xs-12">
                                     <div class="form-group">
                                         <button type="submit" class="theme-btn">Enviar</button>
                                     </div>
                                 </div> -->
+                            </div>
+                            
+                                    <div class="form-group">
+                                        <textarea name="message" placeholder="Mensagem"><?= isset($_GET['produto'])?"Boa Tarde.\n\nGostaria de receber um orçamento referente ao produto ".explode("-",$_GET['produto'])[1]." com as seguintes especificações:\n\nObrigado(a).":'' ?></textarea>
+                                    </div>
+                            
+                            <div class="form-group">
+                                <button type="submit" class="theme-btn">Enviar</button>
                             </div>
                         </form>
                     </div>
