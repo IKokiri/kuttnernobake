7f5792c65f6abb4113a8d454779e59d7fe4309dd
Luiz Mendes <developer.luizmendes@gmail.com>
2019-08-16 15:20:20 -0300

    adição de semantica

--- resources/views/site/components/component_highlights.blade.php
+++ resources/views/site/components/component_highlights.blade.php
@@ -5,25 +5,33 @@
             <div class="highlight-block">
                 <div class="inner-box">
                     <div class="icon-box"><span class="machinery-icon-factory"></span></div>
-                    <div class="text">Incorporada no <span> GRUPO Kuttner</span> como 
-                    afiliada da Kuttner do Brasil em Junho de 2017.</div>
+                    <div class="text">
+                        Incorporada no 
+                        <a target="_blank" href="https://www.kuettner.com/en/about-us">
+                            <strong><span>Grupo Kuttner<span></strong> 
+                        </a>    
+                        como afiliada da 
+                        <a target="_blank" href="http://www.kuttner.com.br">
+                            <strong><span>Kuttner do Brasil<span></strong> 
+                        </a>    
+                        em Junho de 2017.</div>
                 </div>
             </div>
-
             <!-- Highlight Block -->
             <div class="highlight-block">
                 <div class="inner-box">
                     <div class="icon-box"><span class="machinery-icon-factory"></span></div>
                     <div class="text">A linha de produtos da  
-                        <span> Kuttner No-Bake Solutions</span> é complementada pela linha de produtos tradicionais de Kuttner do Brasil.</div>
+                        <span><strong>Kuttner No-Bake Solutions</strong></span> é complementada pela linha de produtos tradicionais da <a target="_blank" href="http://www.kuttner.com.br">
+                            <strong><span>Kuttner do Brasil<span></strong> 
+                        </a>.</div>
                 </div>
             </div>
-
             <!-- Highlight Block -->
             <div class="highlight-block">
                 <div class="inner-box">
                     <div class="icon-box"><span class="machinery-icon-factory"></span></div>
-                    <div class="text">A Kuttner   <span> No-Bake Solutions</span> encontra-se instalada em sede própria em Piracicaba / SP.</div>
+                    <div class="text">A Kuttner <strong><span>Kuttner No-Bake Solutions</span></strong> encontra-se instalada em sede própria em Piracicaba / SP.</div>
                 </div>
             </div>
         </div>
